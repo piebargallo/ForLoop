ffe1a5c2c21ad54b5b8c99d0655dd6a9abc24b48
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-08-21 11:17:02 +0200

    Add files via upload

---
+++ Exam2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Driver code 
+int main () {
+
+	// Def local variable
+   int a;
+	
+   //for loop execution 
+   for( a = 11; a <= 16; a = a + 1 )
+      
+	  printf("value of a: %d\n", a);
+   
+ 	return 0;
+
+} // End driver
