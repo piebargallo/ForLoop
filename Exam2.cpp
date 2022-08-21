523e7d1e9fcb1f8b25b6bdef23c201a160f66a49
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-08-21 11:18:57 +0200

    Update Exam2.cpp

--- Exam2.cpp
+++ Exam2.cpp
@@ -3,13 +3,13 @@
 // Driver code 
 int main () {
 
-	// Def local variable
+   // Def local variable
    int a;
 	
    //for loop execution 
    for( a = 11; a <= 16; a = a + 1 )
       
-	  printf("value of a: %d\n", a);
+  	printf("value of a: %d\n", a);
    
  	return 0;
 
